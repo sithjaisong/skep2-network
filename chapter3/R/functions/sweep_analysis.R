abe8bd1973c8119cfee0f0bedb83c69ad50db344
Sith Jaisong <sith.jaisong@gmail.com>
2016-08-29 08:21:52 +0700

    fix

--- chapter3/R/functions/sweep_analysis.R
+++
@@ -1,9 +0,0 @@
-
-sweep_anlaysis <- function(sweep) {
-  
-  result <- sweep %>% dplyr::group_by(index, Country, Year, Season, Fieldno, visit, DVS) %>%
-    dplyr::summarise(m.GLH = mean(GLH.sweep)) %>%
-    dplyr::group_by(index, Country, Year, Season, Fieldno) %>% 
-    dplyr::summarise(GLH.audpc = audpc(m.GLH, DVS))
-  
-}
